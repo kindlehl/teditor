f18ef715aeda70ec75e285e84187fa57544abbb7
kindlehl <kindlehl@osuosl.org>
2018-09-17 16:27:52 -0700

    moved functions for input handling into separate CU

--- src/main.c
+++ src/main.c
@@ -6,16 +6,16 @@
 #include <SDL2/SDL_image.h>
 #include "../include/screen.h"
 #include "../include/palette.h"
+#include "../include/input.h"
 
 #define TRUE 1
 #define FALSE 0
 #define MAX_FILES 20
 
-typedef int BOOL;
-
 char* paths[MAX_FILES] = { NULL };
 const int FORMATS = 7;
-BOOL run = TRUE;
+int run = TRUE;
+char filename[256];
 SDL_Window* main_window;
 
 int stringToInt(char* s) {
@@ -40,7 +40,7 @@ int setTexturePaths() {
 
 	while( (file = readdir(CWD)) ) {
 		if( file->d_type == DT_REG ){
-			BOOL acceptable_format = FALSE;
+			int acceptable_format = FALSE;
 			for(int i = 0; i < FORMATS; i++){
 
 				if (strstr(file->d_name, supported_formats[i])) {
@@ -75,69 +75,6 @@ int setTexturePaths() {
 void freePaths(char** paths){
 	for(int i = 0; i < MAX_FILES; i++){
 		free(paths[i]);
-	}
-}
-
-void handleMouse( struct Screen* screen, struct Palette* palette,  SDL_Event* e){
-
-	int screen_ID = SDL_GetWindowID(main_window);
-	int palette_ID = SDL_GetWindowID(palette->window);
-
-	if( !(screen_ID && palette_ID)  ){
-		printf("Error gathering window IDs: %s\n", SDL_GetError());
-	}
-
-	if(e->type == SDL_MOUSEBUTTONDOWN){
-
-		int x = e->button.x,
-			y = e->button.y,
-			w = palette->tiles[0].rect.w,
-			h = palette->tiles[0].rect.h,
-			rows = palette->rows,
-			cols = palette->cols;
-
-		if(e->button.windowID == palette_ID){
-
-			//not sure why the -1 is needed in this scenario...
-			palette->selected = ( (y / h) * cols ) + x / w;
-			printf("clicked palette item number %d", palette->selected);
-			fflush(stdout);
-
-		}else{
-		
-			int index = palette->selected;
-			//causes memory leak.
-			struct Tile* screen_tile = screenGetTile(screen, x, y);
-			tileDestroy(screen_tile);
-			tileInitFromSurface(screen_tile, palette->tiles[index].path, screen_tile->rect, screen_tile->renderer, 0);
-
-
-		}
-
-	}else if(e->type == SDL_WINDOWEVENT){
-
-	//Makes any window you mouse over active. This prevents the user from needing to click
-	//anytime he wants to go between the two windows
-		if(e->window.event == SDL_WINDOWEVENT_ENTER){
-			SDL_SetWindowInputFocus(SDL_GetWindowFromID(e->window.windowID));
-		}
-
-	}
-}
-
-void processEvents( struct Screen* screen, struct Palette* palette){
-	SDL_Event e;
-	while(SDL_PollEvent(&e)){
-		switch(e.type){
-			case SDL_QUIT: run = FALSE;
-				break;
-			case SDL_MOUSEMOTION:
-			case SDL_MOUSEBUTTONDOWN:
-			case SDL_MOUSEBUTTONUP:
-			case SDL_WINDOWEVENT:
-				handleMouse(screen, palette, &e);
-				break;
-		}
 	}
 }
 
@@ -176,8 +113,10 @@ int main(int argc, char** argv) {
 
 	screenInit(&screen, main_renderer, "/home/pepe/projects/teditor/default.jpg", w, h, 100, 100);
 	paletteInit(&palette, paths, num_textures);
+	//initialized by setTexturePaths
+	freePaths(paths);
 	
-	while( run ){
+	while( run == TRUE ){
 		SDL_SetRenderDrawColor(main_renderer, 255, 0, 0, 255);
 		SDL_SetRenderDrawColor(palette.renderer, 255, 0, 0, 255);
 
@@ -190,7 +129,6 @@ int main(int argc, char** argv) {
 		SDL_RenderPresent(palette.renderer);
 	}
 	
-	freePaths(paths);
 	screenDestroy(&screen);
 	paletteDestroy(&palette);
 	SDL_DestroyRenderer(main_renderer);
