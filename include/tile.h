24a6ea3c42322a5bf98fe973e329afd7b5c0072e
kindlehl <kindlehl@osuosl.org>
2018-09-16 23:13:53 -0700

    minor rehaul of init function
    Changed texture initailization to use the SDL2_image library and a path to an image

--- include/tile.h
+++ include/tile.h
@@ -5,12 +5,13 @@
 
 struct Tile {
 	int id;
+	char* path;
 	struct SDL_Rect rect;
 	struct SDL_Renderer* renderer; //This pointer is not owned by the Tile struct.
 	struct SDL_Texture* texture;
 };
 
-void tileInitFromSurface(struct Tile*, struct SDL_Rect, struct SDL_Surface*, struct SDL_Renderer*, int);
+void tileInitFromSurface(struct Tile*, char* path, struct SDL_Rect, struct SDL_Renderer*, int);
 void tileDestroy(struct Tile*);
 void tileChangeDimensions(struct Tile*, struct SDL_Rect);
 void tileRender(struct Tile*);
--- src/tile.c
+++ src/tile.c
@@ -1,14 +1,26 @@
 #include "../include/tile.h"
+#include <SDL2/SDL_image.h>
 
-void tileInitFromSurface(struct Tile* tile, struct SDL_Rect rect, struct SDL_Surface* surface, struct SDL_Renderer* renderer, int id){
+void tileInitFromSurface(struct Tile* tile, char* path, struct SDL_Rect rect, struct SDL_Renderer* renderer, int id){
 	tile->rect = rect;
-	tile->texture = SDL_CreateTextureFromSurface(renderer, surface);
+	tile->path = (char*)malloc(strlen(path) + 1);
+	strcpy(tile->path, path);
+	SDL_Surface* tile_surface = IMG_Load(path);
+	if (!tile_surface) {
+		printf("Error loading surface with image: %s\n", path);
+	}
+	tile->texture = SDL_CreateTextureFromSurface(renderer, tile_surface);
+	if (!tile->texture) {
+		printf("Error loading texture from surface using image: %s\n", path);
+	}
+	SDL_FreeSurface(tile_surface);
 	tile->renderer = renderer;
 	id = id;
 }
 
 void tileDestroy(struct Tile* tile){
 	SDL_DestroyTexture(tile->texture);
+	free(tile->path);
 }
 
 //probably useless
